c845d3111d66588119c6b5be11fefc551f2621ec
UnrealSec <arran.bishop89@aol.com>
2020-07-06 09:12:53 +0300

    Add example

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using OpenDirDump;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        OpenDirDumper dumper = new OpenDirDumper(target => {
+            if (target.BaseUrl.Host != target.Url.Host) return;
+
+            target.Scrape = target.Response.ContentType.Contains("text/html");
+            Console.WriteLine(target.Url.ToString());
+        });
+
+        dumper.Dump("https://index-of.es/");
+
+        Console.ReadLine();
+    }
+}
